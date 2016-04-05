195b7dcf7e6762cd0698e79c950d7ae08024187f
James Valaitis <james@infinityjames.com>
2016-04-05 13:16:49 +0100

    Initial commit.

---
+++ GymCounter/Controller/ExerciseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  GymCounter
+//
+//  Created by James Valaitis on 05/04/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: Exercise Table View Controller Class
+
+class ExterciseTableViewController: UIViewController {
+}
---
+++ GymCounter/Supporting-Files/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GymCounter
+//
+//  Created by James Valaitis on 05/04/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: App Delegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    //	MARK: Properties
+
+    var window: UIWindow?
+
+    //	MARK: UIApplicationDelegate
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
