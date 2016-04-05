f78408e5453fa3345af39fdd9efe6f559257e0b4
James Valaitis <james@infinityjames.com>
2016-04-05 17:57:27 +0100

    Added measurement unit enum.

--- GymCounter/Model/Exercise.swift
+++ GymCounter/Model/Exercise.swift
@@ -26,8 +26,31 @@ struct Exercise {
 
 struct Set {
     
+    //	MARK: Measurement Unit Enum
+    
+    enum MeasurementUnit {
+        /// An imperial measurement unit (lbs).
+        case Imperial
+        /// A metric measurement unit (kg).
+        case Metric
+        
+        /// This measurement unit as a unit to meaure mass (as a String).
+        var massUnitString: String {
+            switch self {
+            case .Imperial:
+                return "lbs"
+            case .Metric:
+                return "kg"
+            }
+        }
+    }
+    
     //	MARK: Properties
     
     /// The number of reps performed in the set.
     let reps: Int
+    /// The weight lifted in this set.
+    let weight: Double
+    /// The measurement unit used for the weight lifted.
+    let weightMeasurementUnit: MeasurementUnit
 }
