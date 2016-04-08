5412b88c24271764774d0ffbf946fd23c20499a0
James Valaitis <james@infinityjames.com>
2016-04-08 12:58:31 +0100

    Added a view to allow the user to change the weight they lifted in a set.

--- GymCounter/Controller/ExerciseCounterViewController.swift
+++ GymCounter/Controller/ExerciseCounterViewController.swift
@@ -26,11 +26,29 @@ class ExerciseCounterViewController: UIViewController {
     @IBOutlet private var repsLabel: UILabel!
     /// The counter used to increment or decrement the reps for a set.
     @IBOutlet private var repsCounter: UIStepper!
+    /// Button to set the unit of measurement for the mass lifted.
+    @IBOutlet private var unitButton: MassUnitButton!
+    /// The text field which provides the user with a way to enter the mass lifted.
+    @IBOutlet private var weightTextField: UITextField!
     
     //	MARK: Actions
     
     @IBAction private func counterValueChanged(counter: UIStepper) {
         repsLabel.text = "\(Int(counter.value))"
+    }
+    
+    @IBAction private func addSetTapped(addButton: UIButton) {
+        guard let reps = Int(repsLabel.text ?? ""),
+        weight =  Double(weightTextField.text ?? "") else {
+            let repsErrorMessage = "The reps label should only ever display a number and should be convertible to Int: \(repsLabel.text)"
+            let weightErrorMessage = "The weight text field should only ever contain a number: \(weightTextField.text)"
+            let fullErrorMessage = repsErrorMessage + "\n" + weightErrorMessage
+            fatalError(fullErrorMessage)
+        }
+        
+        let set = Set(reps: reps, weight: weight, weightMeasurementUnit: unitButton.selectedMeasurementUnit)
+        exercise?.sets.append(set)
+        
     }
     
     //	MARK: UI Functions
