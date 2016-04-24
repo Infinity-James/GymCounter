32b837d2a854f1fa9d9dc7a64b1f7273af1ba2b0
James Valaitis <james@infinityjames.com>
2016-04-24 15:54:43 +0100

    Implemented the data source for the colelction view.

--- GymCounter/Model/SetsCollectionViewDataSource.swift
+++ GymCounter/Model/SetsCollectionViewDataSource.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2016 &Beyond. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//	MARK: Sets Collection View Data Source
+
+/**
+    `SetsCollectionViewDataSource`
+ 
+    A data source for the colleciton view which displays the sets performed in an exercise.
+ */
+class SetsCollectionViewDataSource: NSObject {
+    
+    //	MARK: Property
+    
+    /// The collection view that we provide the data for.
+    weak var collectionView: UICollectionView?
+    //  Reuse identifier for the cells.
+    private let reuseIdentifier = String(SetCollectionViewCell)
+    //  The exercise whose sets need to be displayed.
+    var exercise: Exercise {
+        didSet {
+            collectionView?.reloadData()
+        }
+    }
+    
+    //	MARK: Initialization
+    
+    init(withExercise exercise: Exercise = Exercise(sets: [], name: "", repTarget: 8)) {
+        self.exercise = exercise
+        
+        super.init()
+    }
+}
+
+extension SetsCollectionViewDataSource: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as? SetCollectionViewCell else {
+            fatalError("Could not dequeue a cell with the reuse identifier \(reuseIdentifier).")
+        }
+        
+        let set = exercise.sets[indexPath.item]
+        cell.set = set
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return exercise.sets.count
+    }
+}
