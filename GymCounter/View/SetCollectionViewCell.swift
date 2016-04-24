bd6bd3a2da93672f684183341a54329075442825
James Valaitis <james@infinityjames.com>
2016-04-24 15:52:39 +0100

    Added two labels to the cell.

--- GymCounter/View/SetCollectionViewCell.swift
+++ GymCounter/View/SetCollectionViewCell.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
+//	MARK: Set Collection View Cell Class
+
+/**
+    `SetCollectionViewCell`
+ 
+    A cell which represents a set performed of an exercise.
+ */
 class SetCollectionViewCell: UICollectionViewCell {
     
+    //	MARK: Properties
+    
+    //  The set to be displayed in this cell.
+    var set: Set? {
+        didSet {
+            guard let set = set else { return }
+            
+            weightLabel.text = String(set.weight)
+            repsLabel.text = String(set.reps)
+        }
+    }
+    //  The label that displays the weight performed in the set.
+    @IBOutlet private var weightLabel: UILabel!
+    //  The label that displays the reps performed in the set.
+    @IBOutlet private var repsLabel: UILabel!
 }
