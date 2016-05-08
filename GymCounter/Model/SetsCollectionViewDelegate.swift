0c866e450a33cc5169eb5fa71da5b986e725235e
James Valaitis <james@infinityjames.com>
2016-05-08 18:39:26 +0100

    Created objects for laying out the sets collection view and to act as the delegate of it

---
+++ GymCounter/Model/SetsCollectionViewDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SetsCollectionViewDelegate.swift
+//  GymCounter
+//
+//  Created by James Valaitis on 08/05/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
---
+++ GymCounter/Model/SetsCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  SetsCollectionViewLayout.swift
+//  GymCounter
+//
+//  Created by James Valaitis on 08/05/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+class SetsCollectionViewLayout: UICollectionViewFlowLayout {
+
+}
